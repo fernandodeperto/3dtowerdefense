051259ccac83505116aded2d79dc7f2299bfb3c5
Fernando Mendonca <foxdie86@gmail.com>
2018-03-04 22:35:07 -0300

    Adiciona os waypoints
    - Fim do video 2

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float _speed = 10f;
+
+    private Transform __target;
+    private int __waypointIndex = 0;
+    private float __minPosition = 0.5f;
+
+    void Start()
+    {
+        __target = Waypoints._points[0];
+    }
+
+    void Update()
+    {
+        Vector3 direction = __target.position - transform.position;
+
+        transform.Translate(direction.normalized * _speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, __target.position) <= __minPosition)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (__waypointIndex >= Waypoints._points.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            __target = Waypoints._points[++__waypointIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+    public static Transform[] _points;
+
+    void Awake() {
+        _points = new Transform[transform.childCount];
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = transform.GetChild(i);
+        }
+	}
+}
