9402766c6d7ec0c5c84ee3224437bea96bebdf14
Fernando Mendonca <foxdie86@gmail.com>
2018-03-04 22:50:46 -0300

    Testa a criacao de inimigos

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -1,16 +1,21 @@
 ﻿using UnityEngine;
 
-public class EnemySpawn : MonoBehaviour {
+public class EnemySpawn : MonoBehaviour
+{
     public Transform _enemyPrefab;
+    public Transform _spawnPoint;
     public float _timeBetweenWaves = 5f;
 
     private float __countdown = 2f;
+    private int __waveIndex = 1;
 
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
-	void Update () {
+	void Update ()
+    {
 		if (__countdown <= 0f)
         {
             SpawnWave();
@@ -23,6 +28,16 @@ public class EnemySpawn : MonoBehaviour {
 
     void SpawnWave()
     {
-        Debug.Log("wave incoming");
+        for (int i = 0; i < __waveIndex; i++)
+        {
+            SpawnEnemy();
+        }
+
+        //__waveIndex++;
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(_enemyPrefab, _spawnPoint.position, _spawnPoint.rotation);
     }
 }
