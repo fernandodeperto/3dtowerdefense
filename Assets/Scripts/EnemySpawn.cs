aea067b000e55ded5f0bc1a4c1ca76f70544a5fb
Fernando Mendonca <foxdie86@gmail.com>
2018-03-04 22:42:21 -0300

    Testa o lancamento de waves

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+    public Transform _enemyPrefab;
+    public float _timeBetweenWaves = 5f;
+
+    private float __countdown = 2f;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (__countdown <= 0f)
+        {
+            SpawnWave();
+
+            __countdown = _timeBetweenWaves;
+        }
+
+        __countdown -= Time.deltaTime;
+	}
+
+    void SpawnWave()
+    {
+        Debug.Log("wave incoming");
+    }
+}
