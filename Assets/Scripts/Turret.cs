65c772a09f07739a04ea695080515656e1ba719a
Fernando Mendonca <foxdie86@gmail.com>
2018-03-08 00:20:58 -0300

    Testa o movimento da bala

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -8,8 +8,12 @@ public class Turret : MonoBehaviour
     public float _updatePeriod = 0.2f;
     public Transform _rotate;
     public float _rotationSpeed = 6f;
+    public float _fireRate = 1f;
+    public GameObject _bulletPrefab;
+    public Transform _firePoint;
 
     private Transform __target;
+    private float __fireCountdown = 0f;
 
 	void Start()
     {
@@ -27,6 +31,16 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(direction);
         Vector3 rotation = Quaternion.Lerp(_rotate.rotation, lookRotation, Time.deltaTime * _rotationSpeed).eulerAngles;
         _rotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        if (__fireCountdown <= Mathf.Epsilon)
+        {
+            Shoot();
+            __fireCountdown = 1f / _fireRate;
+        }
+        else
+        {
+            __fireCountdown -= Time.deltaTime;
+        }
 	}
 
     void UpdateTarget()
@@ -60,5 +74,16 @@ public class Turret : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, _range);
+    }
+
+    void Shoot()
+    {
+        GameObject bulletGameObject = (GameObject)Instantiate(_bulletPrefab, _firePoint.position, _firePoint.rotation);
+        Bullet bullet = bulletGameObject.GetComponent<Bullet>();
+
+        if (bullet != null)
+        {
+            bullet.SetTarget(__target);
+        }
     }
 }
