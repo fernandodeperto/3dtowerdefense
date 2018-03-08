266c7d0e856b2cd892cee0cf52cb622db65d975e
Fernando Mendonca <foxdie86@gmail.com>
2018-03-08 20:03:21 -0300

    Adiciona o script de waypoints

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+    public static Transform[] _points;
+
+    void Awake() {
+        _points = new Transform[transform.childCount];
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = transform.GetChild(i);
+        }
+	}
+}
