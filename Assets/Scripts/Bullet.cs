997bccf8f090b7c6cc6120227d86a181e210047e
Fernando Mendonca <foxdie86@gmail.com>
2018-03-08 00:37:45 -0300

    Testa o efeito de particulas da bala

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+    public float _speed = 70f;
+    public GameObject _bulletImpact;
+    private float __effectDuration = 2f;
+
+    private Transform __target;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if (__target == null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Vector3 direction = __target.position - transform.position;
+            float moveDistance = _speed * Time.deltaTime;
+
+            if (direction.magnitude <= moveDistance)
+            {
+                HitTarget();
+            }
+            else
+            {
+                transform.Translate(direction.normalized * moveDistance, Space.World);
+            }
+        }
+
+	}
+
+    public void SetTarget(Transform target)
+    {
+        __target = target;
+    }
+
+    void HitTarget()
+    {
+        GameObject effectGameObject = Instantiate(_bulletImpact, transform.position, transform.rotation);
+
+        Destroy(effectGameObject, __effectDuration);
+        Destroy(gameObject);
+        Destroy(__target.gameObject);
+    }
+}
