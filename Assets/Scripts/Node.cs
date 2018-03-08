a86bc000e2ff638ff8105237bc2d3bfcdd3ac5ac
Fernando Mendonca <foxdie86@gmail.com>
2018-03-08 00:48:12 -0300

    Testa o efeito de mudanca de cor dos tiles

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color _hoverColor;
+
+    private Renderer __renderer;
+    private Color __originalColor;
+
+	void Start()
+    {
+        __renderer = GetComponent<Renderer>();
+        __originalColor = __renderer.material.color;
+	}
+	
+	void Update()
+    {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        __renderer.material.color = _hoverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        __renderer.material.color = __originalColor;
+    }
+}
