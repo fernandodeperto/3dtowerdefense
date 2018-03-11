5edb8193ccac250ba66265a4b9b339851cdd1b30
Fernando Mendonca <foxdie86@gmail.com>
2018-03-11 01:39:37 -0300

    Configura o BuildManager

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+    public static BuildManager _instance;
+    public GameObject _standardTurret;
+
+    private GameObject __selectedTurret;
+
+    void Awake()
+    {
+        if (_instance != null)
+        {
+            Debug.Log("build manager already exists");
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    void Start()
+    {
+        __selectedTurret = _standardTurret;
+    }
+
+    public GameObject GetSelectedTurret()
+    {
+        return __selectedTurret;
+    }
+}
